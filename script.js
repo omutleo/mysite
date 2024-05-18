35dcbba43550d252912d9d9755b7912a53a4b807
omutleo <prorokkant@yandex.ru>
2024-05-18 17:26:46 +0300

    Update script.js
    dlkjfg
    
    Signed-off-by: omutleo <prorokkant@yandex.ru>

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const sections = document.querySelectorAll('.section');
 
 navLinks.forEach((link, index) => {
   link.addEventListener('click', (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    const targetSection = sections[index];
-    $('html, body').animate({
-      scrollTop: $(targetSection).offset().top
-    }, 1300); // отрегулируйте длительность анимации по необходимости
+    event.preventDefault(); // Предотвращаем стандартное поведение ссылки
+
+    const targetId = link.getAttribute('href'); // Получаем ID секции, к которой нужно перейти
+    const targetSection = document.querySelector(targetId); // Находим секцию по ID
+
+    // Плавная прокрутка к секции
+    targetSection.scrollIntoView({
+      behavior: 'smooth' // Добавляем плавность
+    });
   });
 });
+
