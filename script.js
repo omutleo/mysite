50ee6202679d71dbe2c15d0de7fc452b791166da
omutleo <prorokkant@yandex.ru>
2024-05-18 17:10:03 +0300

    Update script.js
    kdjsgh
    
    Signed-off-by: omutleo <prorokkant@yandex.ru>

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-const navLinks = document.querySelectorAll('.nav-link');
-const sections = document.querySelectorAll('.section');
-
 navLinks.forEach((link, index) => {
   link.addEventListener('click', (event) => {
-    event.preventDefault();
-    event.stopPropagation(); // Остановка дефолтного поведения ссылки
+    event.stopPropagation();
+    event.stopImmediatePropagation();
     sections.forEach((section) => {
-      section.style.animation = 'none'; // Останавливаем текущую анимацию
-      section.offsetHeight; // Триггер перерисовки
-      section.style.animation = 'fade-in 2s ease-in-out forwards'; // Запускаем анимацию заново
+      section.style.animation = 'none';
+      section.offsetHeight;
+      section.style.animation = 'fade-in 2s ease-in-out forwards';
     });
     const targetSection = sections[index];
     targetSection.scrollIntoView({ behavior: 'smooth' });
-    return false; // Остановка дефолтного поведения ссылки
   });
 });
-
