5169b47b71f8adc955aa9ab48cbcc6fecf30d264
omutleo <prorokkant@yandex.ru>
2024-06-26 16:44:35 +0300

    Update script.js
    Signed-off-by: omutleo <prorokkant@yandex.ru>

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const navLinks = document.querySelectorAll('.nav-link');
   const sections = document.querySelectorAll('.section');
 
-  navLinks.forEach((link) => {
+  navLinks.forEach((link, index) => {
     link.addEventListener('click', (event) => {
       event.preventDefault(); // Предотвращаем стандартное поведение ссылки
 
@@ -25,5 +25,24 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
- 
+  const backToTopButtons = document.querySelectorAll('.back-to-top');
 
+  window.addEventListener('scroll', () => {
+    backToTopButtons.forEach((button) => {
+      if (window.pageYOffset > 100) {
+        button.classList.add('show');
+      } else {
+        button.classList.remove('show');
+      }
+    });
+  });
+
+  backToTopButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+  });
+});
