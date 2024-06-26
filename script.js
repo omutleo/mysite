b30e379065ff68d8f8c7a8ddef6149e8e8eb1ac5
omutleo <prorokkant@yandex.ru>
2024-06-26 16:39:23 +0300

    Update script.js
    Signed-off-by: omutleo <prorokkant@yandex.ru>

--- script.js
+++ script.js
@@ -25,21 +25,5 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
-  const backToTopButton = document.querySelector('.back-to-top');
-
-  window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 100) {
-      backToTopButton.classList.add('show');
-    } else {
-      backToTopButton.classList.remove('show');
-    }
-  });
-
-  backToTopButton.addEventListener('click', () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  });
-});
+ 
 
