d8d6015ee6fb44b4a88d4112f0bce780dc518d0a
omutleo <prorokkant@yandex.ru>
2024-06-26 10:41:27 +0300

    Update script.js
    Signed-off-by: omutleo <prorokkant@yandex.ru>

--- script.js
+++ script.js
@@ -1,40 +1,3 @@
-const navLinks = document.querySelectorAll('.nav-link');
-const sections = document.querySelectorAll('.section');
-
-navLinks.forEach((link, index) => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault(); // Предотвращаем стандартное поведение ссылки
-
-    const targetId = link.getAttribute('href'); // Получаем ID секции, к которой нужно перейти
-    const targetSection = document.querySelector(targetId); // Находим секцию по ID
-
-    // Плавная прокрутка к секции
-    targetSection.scrollIntoView({
-      behavior: 'smooth' // Добавляем плавность
-    });
-  });
-});
-const backToTopButtons = document.querySelectorAll('.back-to-top');
-
-window.addEventListener('scroll', () => {
-  backToTopButtons.forEach((button) => {
-    if (window.pageYOffset > 100) {
-      button.classList.add('show');
-    } else {
-      button.classList.remove('show');
-    }
-  });
-});
-
-backToTopButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  });
-});
-// script.js
 document.addEventListener("DOMContentLoaded", function() {
   const videoContainer = document.getElementById('video-container');
   const content = document.getElementById('content');
@@ -44,6 +7,42 @@ document.addEventListener("DOMContentLoaded", function() {
     content.style.display = 'block';
     document.body.style.overflow = 'auto';
   });
+
+  const navLinks = document.querySelectorAll('.nav-link');
+  const sections = document.querySelectorAll('.section');
+
+  navLinks.forEach((link, index) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault(); // Предотвращаем стандартное поведение ссылки
+
+      const targetId = link.getAttribute('href'); // Получаем ID секции, к которой нужно перейти
+      const targetSection = document.querySelector(targetId); // Находим секцию по ID
+
+      // Плавная прокрутка к секции
+      targetSection.scrollIntoView({
+        behavior: 'smooth' // Добавляем плавность
+      });
+    });
+  });
+
+  const backToTopButtons = document.querySelectorAll('.back-to-top');
+
+  window.addEventListener('scroll', () => {
+    backToTopButtons.forEach((button) => {
+      if (window.pageYOffset > 100) {
+        button.classList.add('show');
+      } else {
+        button.classList.remove('show');
+      }
+    });
+  });
+
+  backToTopButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+  });
 });
-
-
