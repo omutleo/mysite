6c4193fdfc4fb62e00c88ded2e339be693bd9cd3
omutleo <prorokkant@yandex.ru>
2024-06-27 17:37:28 +0300

    Update script.js
    Signed-off-by: omutleo <prorokkant@yandex.ru>

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
   const videoContainer = document.getElementById('video-container');
   const content = document.getElementById('content');
   const navLinks = document.querySelectorAll('.nav-link');
@@ -23,56 +23,6 @@
       targetSection.scrollIntoView({
         behavior: 'smooth' // Плавная прокрутка к секции
       });
-    });
-  });
-});
-*/
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("DOM fully loaded and parsed");
-
-  const videoContainer = document.getElementById('video-container');
-  const content = document.getElementById('content');
-  const navLinks = document.querySelectorAll('.nav-link');
-  const scrollToTopBtn = document.getElementById('scrollToTopBtn');
-
-  if (scrollToTopBtn) {
-    console.log("Scroll to top button found");
-  } else {
-    console.log("Scroll to top button not found");
-  }
-
-  // Обработчик клика по видео контейнеру
-  videoContainer.addEventListener('click', function() {
-    videoContainer.style.display = 'none';
-    content.style.display = 'block';
-    document.body.style.overflow = 'auto';
-  });
-
-  // Обработчики кликов по навигационным ссылкам и прокрутка к секциям
-  navLinks.forEach((link) => {
-    link.addEventListener('click', (event) => {
-      event.preventDefault();
-      const targetId = link.getAttribute('href');
-      document.querySelector(targetId).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
-
-  // Показ и скрытие кнопки "Наверх"
-  window.addEventListener('scroll', function() {
-    if (window.scrollY > 300) {
-      scrollToTopBtn.style.display = 'block';
-    } else {
-      scrollToTopBtn.style.display = 'none';
-    }
-  });
-
-  // Обработчик клика по кнопке "Наверх"
-  scrollToTopBtn.addEventListener('click', function() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
     });
   });
 });
