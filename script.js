7a2ad9485e5940dbc488a6211fe0a2f232b889f0
omutleo <prorokkant@yandex.ru>
2024-06-27 17:26:55 +0300

    Update script.js
    Signed-off-by: omutleo <prorokkant@yandex.ru>

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+/*document.addEventListener("DOMContentLoaded", function() {
   const videoContainer = document.getElementById('video-container');
   const content = document.getElementById('content');
   const navLinks = document.querySelectorAll('.nav-link');
@@ -26,5 +26,50 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 });
+*/
+document.addEventListener("DOMContentLoaded", function() {
+  const videoContainer = document.getElementById('video-container');
+  const content = document.getElementById('content');
+  const navLinks = document.querySelectorAll('.nav-link');
+  const sections = document.querySelectorAll('.section');
+  const scrollToTopBtn = document.getElementById('scrollToTopBtn');
 
+  // Обработчик клика по видео контейнеру
+  videoContainer.addEventListener('click', function() {
+    videoContainer.style.display = 'none'; // Скрытие видео контейнера
+    content.style.display = 'block'; // Отображение основного контента
+    document.body.style.overflow = 'auto'; // Включение прокрутки страницы
+  });
 
+  // Обработчики кликов по навигационным ссылкам
+  navLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault(); // Предотвращаем стандартное поведение ссылки
+
+      const targetId = link.getAttribute('href'); // Получаем ID секции, к которой нужно перейти
+      const targetSection = document.querySelector(targetId); // Находим секцию по ID
+
+      // Плавная прокрутка к секции
+      targetSection.scrollIntoView({
+        behavior: 'smooth' // Плавная прокрутка к секции
+      });
+    });
+  });
+
+  // Обработчик прокрутки для показа/скрытия кнопки "Наверх"
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 300) {
+      scrollToTopBtn.style.display = 'block';
+    } else {
+      scrollToTopBtn.style.display = 'none';
+    }
+  });
+
+  // Обработчик клика по кнопке "Наверх"
+  scrollToTopBtn.addEventListener('click', function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+});
