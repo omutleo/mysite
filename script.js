7247ae4e3640d7510f6e980dc0c8df9457d1adc9
omutleo <prorokkant@yandex.ru>
2024-06-27 17:20:06 +0300

    Update script.js
    Signed-off-by: omutleo <prorokkant@yandex.ru>

--- script.js
+++ script.js
@@ -27,3 +27,22 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+// Получаем кнопку "наверх"
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Функция показа/скрытия кнопки в зависимости от положения скролла
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block"; // Показываем кнопку
+  } else {
+    scrollToTopBtn.style.display = "none"; // Скрываем кнопку
+  }
+}
+
+// Функция прокрутки наверх при клике на кнопку
+scrollToTopBtn.addEventListener("click", function() {
+  document.body.scrollTop = 0; // Для Safari
+  document.documentElement.scrollTop = 0; // Для других браузеров
+});
