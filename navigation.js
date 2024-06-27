8f912c412ed43d2fa161ae61a8652e46d9c8abcd
omutleo <prorokkant@yandex.ru>
2024-06-27 12:54:17 +0300

    Create navigation.js
    Signed-off-by: omutleo <prorokkant@yandex.ru>

---
+++ navigation.js
@@ -0,0 +1,36 @@
+// navigation.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  const navLinks = document.querySelectorAll('.nav-link');
+  const sections = document.querySelectorAll('.section');
+  const backToTopButton = document.querySelector('.back-to-top');
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault(); // Предотвращаем стандартное поведение ссылки
+
+      const targetId = link.getAttribute('href'); // Получаем ID секции, к которой нужно перейти
+      const targetSection = document.querySelector(targetId); // Находим секцию по ID
+
+      // Плавная прокрутка к секции
+      targetSection.scrollIntoView({
+        behavior: 'smooth' // Добавляем плавность
+      });
+    });
+  });
+
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 100) {
+      backToTopButton.classList.add('show');
+    } else {
+      backToTopButton.classList.remove('show');
+    }
+  });
+
+  backToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+});
