4a48adc4b3226ad99a19ef7ba87875f3f2f7b90f
omutleo <prorokkant@yandex.ru>
2024-07-01 21:52:47 +0300

    Update script.js
    а
    
    Signed-off-by: omutleo <prorokkant@yandex.ru>

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const navLinks = document.querySelectorAll('.nav-link');
-  const scrollToTopBtn = document.getElementById('scroll-to-top');
+function redirectToHome() {
+    window.location.href = 'home.html';
+}
 
-  // Обработчики кликов по навигационным ссылкам
-  navLinks.forEach((link) => {
-    link.addEventListener('click', (event) => {
-      event.preventDefault();
-      const targetId = link.getAttribute('href');
-      const targetSection = document.querySelector(targetId);
-      targetSection.scrollIntoView({ behavior: 'smooth' });
-    });
-  });
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
 
-  // Показываем кнопку "Наверх" при прокрутке
-  const handleScroll = () => {
-    if (window.scrollY > 300) {
-      scrollToTopBtn.classList.add('show');
-    } else {
-      scrollToTopBtn.classList.remove('show');
-    }
-  };
-
-  window.addEventListener('scroll', handleScroll);
-
-  // Обработчик клика по кнопке "Наверх"
-  scrollToTopBtn.addEventListener('click', () => {
+function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
-  });
+}
 
-  // Вызываем handleScroll сразу после загрузки страницы, чтобы проверить, нужно ли показывать кнопку "Наверх"
-  handleScroll();
-});
+window.onscroll = function() {
+    const scrollButton = document.querySelector('.scroll-button');
+    if (window.scrollY > window.innerHeight) {
+        scrollButton.classList.add('visible');
+    } else {
+        scrollButton.classList.remove('visible');
+    }
+};
