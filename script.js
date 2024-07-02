2799a5ad8d6eee82b25612248d54fd0374553c35
omutleo <prorokkant@yandex.ru>
2024-07-02 12:35:45 +0300

    Update script.js
    Signed-off-by: omutleo <prorokkant@yandex.ru>

--- script.js
+++ script.js
@@ -6,25 +6,3 @@ function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
 }
 
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-
-window.onscroll = function() {
-    const scrollButton = document.querySelector('.scroll-button');
-    const footer = document.querySelector('.footer');
-    const footerRect = footer.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
-
-    if (window.scrollY > window.innerHeight) {
-        scrollButton.classList.add('visible');
-    } else {
-        scrollButton.classList.remove('visible');
-    }
-
-    if (footerRect.top < windowHeight) {
-        scrollButton.style.bottom = `${windowHeight - footerRect.top + 20}px`;
-    } else {
-        scrollButton.style.bottom = '20px';
-    }
-};
