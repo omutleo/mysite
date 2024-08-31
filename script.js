f56c9dd9f9d6f5cac58edbec7cd510475de9e836
omutleo <prorokkant@yandex.ru>
2024-08-31 18:23:27 +0300

    Update script.js
    Signed-off-by: omutleo <prorokkant@yandex.ru>

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-
-
 function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    } else {
+        console.error(`Section with ID ${sectionId} not found.`);
+    }
 }
