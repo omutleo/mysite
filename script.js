2ce7e43549715587f51ab2e32b500ebec7161020
omutleo <prorokkant@yandex.ru>
2024-09-01 12:25:24 +0300

    Update script.js
    Signed-off-by: omutleo <prorokkant@yandex.ru>

--- script.js
+++ script.js
@@ -6,3 +6,46 @@ function scrollToSection(sectionId) {
         console.error(`Section with ID ${sectionId} not found.`);
     }
 }
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("contact-form");
+  const status = document.getElementById("form-status");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const data = new FormData(form);
+
+    fetch("https://formspree.io/f/xwpeygao", {
+      method: "POST",
+      body: data,
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          status.innerHTML = "<p>Спасибо! Ваше сообщение отправлено.</p>";
+          status.style.color = "green";
+          form.reset();
+        } else {
+          response.json().then((data) => {
+            if (Object.hasOwn(data, "errors")) {
+              status.innerHTML = data["errors"]
+                .map((error) => error["message"])
+                .join(", ");
+            } else {
+              status.innerHTML =
+                "Упс! Произошла ошибка при отправке формы.";
+            }
+            status.style.color = "red";
+          });
+        }
+      })
+      .catch((error) => {
+        status.innerHTML = "Упс! Произошла ошибка при отправке формы.";
+        status.style.color = "red";
+      });
+  });
+});
